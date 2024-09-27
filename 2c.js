31985341fcbd8cc3a64227883b231c4d6ea6b37a
jamween <jamielukeconcannon@gmail.com>
2024-09-27 13:51:54 +0000

    2c complete

---
+++ 2c.js
@@ -0,0 +1,7 @@
+let c = (num1, num2)=>{
+    return(num1+num2);
+}
+
+console.log(c(2,1));
+//an arrow function that takes two number and returns the sum of both numbers. Assign the arrow function equal to a variable and get the function to output
+//to the console.
